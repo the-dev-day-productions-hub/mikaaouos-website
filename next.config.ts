98dc6e8bb23661c9065fd9e7625141969db8494e
OPCoderSide <166860374+OPCoderSide@users.noreply.github.com>
2026-04-11 01:56:07 -0700

    feat: added discord.js and api file to grab URL for the profile widget

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
+import { webpack }  from "next/dist/compiled/webpack/webpack";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    webpack: (config) => {
+      config.plugins.push(
+        new webpack.IgnorePlugin({ resourceRegExp: /^zlib-sync$/ }),
+        new webpack.IgnorePlugin({ resourceRegExp: /^bufferutil$/ }),
+    )
+
+    turbopack: {
+      
+    }
+    return config
+  },
 };
 
 export default nextConfig;
