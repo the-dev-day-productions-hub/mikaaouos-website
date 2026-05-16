825ea8559c41775f52fb7a698492788515f678b8
OPCoderSide <166860374+OPCoderSide@users.noreply.github.com>
2026-05-16 01:41:30 -0700

    refactor: api/discord and route.js to route.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,11 @@ import { TbBubbleTea } from "react-icons/tb"
 
 export default async function Home() {
   console.log(`${process.env.APP_URL}/api`)
-  const discordResponse = await fetch(`${process.env.APP_URL}/api`)
+  const discordResponse = await fetch(`${process.env.APP_URL}/api/discord`)
   
   if (!discordResponse.ok) {
     return <div>
-      Failed to fetch data from {process.env.APP_URL}/api
+      Failed to fetch data from {process.env.APP_URL}/api/discord
     </div>
   }
 
