d722445b935c243be9c02a58be6c5e7411d24429
OPCoderSide <166860374+OPCoderSide@users.noreply.github.com>
2026-05-16 03:18:36 -0700

    feat: getting data from supabase

--- src/app/api/guestbook/route.ts
+++ src/app/api/guestbook/route.ts
@@ -1,11 +1,20 @@
-import { SupabaseClient } from "@supabase/supabase-js";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-const supabaseclient = new SupabaseClient(
+const supabaseclient = createClient(
     process.env.PUBLIC_SUPABASE_URL as string,
     process.env.SUPABASE_SECRET as string,
 )
 
 export async function GET() {
+    const {data, error} = await supabaseclient
+        .from('test_table')
+        .select()
     
-    
+    console.log("defalt")    
+    console.log(data)
+
+    return Response.json({
+        message: "helloworld",
+    })
+
 }
